e8334a3312a71fcc78769d2c7be331f1ef4d1f21
Agustin Colombo <agedcolombo@hotmail.com>
2020-02-16 22:58:47 +1100

    authentication and profile

---
+++ app/forms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password',
+                              validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already exists. Please pick another.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email is already linked to an existing account. Please use another.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Sign In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+
+        if user is not None:
+            # and user != current_user.username
+            raise ValidationError('Username already exists. Please pick another.')
---
+++ microblog.py
@@ -0,0 +1,12 @@
+from app import app
+
+
+@app.shell_context_processor
+def make_shell_context():
+    from app import db
+    from app.models import User, Post
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == "__main__":
+    app.run()
