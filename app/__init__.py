00879b17b93b6328b77eea60c341a12645e00f88
Agustin Colombo <agedcolombo@hotmail.com>
2020-03-21 17:38:34 +1100

    Email and local time support

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_babel import Babel
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+moment = Moment(app)
+babel = Babel(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
